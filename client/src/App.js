8c105f9c080fc68c38582dd695e3ed7f41b49aa3
Vannent <erobced@gmail.com>
2022-05-09 17:04:49 -0500

    feat: add auth component to app

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,16 @@ import React from "react";
 import { StreamChat } from "stream-chat";
 import { Chat } from "stream-chat-react";
 import Cookies from "universal-cookie";
-import { ChannelListContainer, ChannelContainer } from "./components";
+import { ChannelListContainer, ChannelContainer, Auth } from "./components";
 import "./App.scss";
 
 const apiKey = "c889pcmm5jwc";
-
 const client = StreamChat.getInstance(apiKey);
 
+const authToken = false;
+
 const App = () => {
+  if (!authToken) return <Auth />;
   return (
     <div className="app_wrapper">
       <Chat client={client}>
