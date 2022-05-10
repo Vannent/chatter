11253219e8752edbf7cab9b8f457589d30ebefc2
Vannent <erobced@gmail.com>
2022-05-10 12:37:48 -0500

    add empty structure

---
+++ client/src/components/UserList.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import { Avatar, useChatChannel } from "stream-chat-react";
+
+const UserList = () => {
+  return <div>UserList</div>;
+};
+
+export default UserList;
